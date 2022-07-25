796740363ed3c0c75a7ccec0278ea0c2ff47771c
PinguMX <105086341+PinguMX@users.noreply.github.com>
2022-07-25 17:47:57 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,54 +4,60 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var ball;
+var ball, leftSide, rightSide, groundObj;
+var world;
+var radius = 40;
 
-function preload()
+/*function preload()
 {
 	
-}
+}*/
 
 function setup() {
 	createCanvas(800, 700);
+	rectMode(CENTER);
 
 	engine = Engine.create();
 	world = engine.world;
 
-	//crear los cuerpos aquí.
-	ball = Bodies.circle(200,100,20,ball_options);
-    World.add(world,ball);
-
     var ball_options={
-		isStatic:false,
-		restitution:0.2,
+		isStatic: false,
+		restitution: 0.3,
 		friction:0,
 		density:1.2
 	}
 
-	Engine.run(engine);
+	 ball = Bodies.circle(260,100,radius/2,ball_options);
+	 World.add(world,ball);
 	
-	ball = Bodies.circle(200,100,20,ball_options);
-	World.add(world,ball);
+
+	//crear los cuerpos aquí.
+	groundObj=new ground(width/2,670,width,20);
+	leftSide = new ground(1100,600,20,120);
+	rightSide = new ground(1350,600,20,120);
+
+	Engine.run(engine);
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-
-    ©
-	groundObj=new ground(width/2,670,40,20);
-	leftSide = new ground(1100,600,20,120)
-
-  drawSprites();
- 
+  
+  //drawSprites();
+ ellipse(ball.position.X,ball.position.Y,radius,radius);
+ groundObj.display();
+ leftSide.display();
+ rightSide.display();
 }
 
-function keyPressed(){
+function keyPressed() {
 	if(keyCode === UP_ARROW) {
-    Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+
+		Matter.body.applyForce(ball,ball.position,{x:85,y:-85});
+
 	}
 }
-
 
 
